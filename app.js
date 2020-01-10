8a20111cbcdb1095e4580553841a98a2a6300a4d
kdenno <kabuyedennis3@gmail.com>
2020-01-10 16:14:13 +0300

    redirects

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 const http = require('http');
-const server = http.createServer((req, res) => {});
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if(url === '/'){
+        res.write('<html>');
+        res.write('<head>');
+        res.write('<body>');
+        res.write('<form action="/message" method="POST><input type="text" name="message"><button type="submit">Submit</button></form>');
+        return res.end();
+    };
+    if(url === '/message'){
+        fs.writeFileSync('msg.txt', 'Dummy data');
+        res.statusCode=302;
+        // Redirect request
+        res.setHeader('Location', '/');
+        return res.end();
+
+    }
+});
 server.listen(8000);

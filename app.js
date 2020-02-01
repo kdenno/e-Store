f863d1c8e28d0c08a4679178792c96c2b29fe5dd
kdenno <kabuyedennis3@gmail.com>
2020-02-01 15:35:01 +0300

    body parsing

--- app.js
+++ app.js
@@ -2,10 +2,20 @@
 const fs = require('fs');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 // execute express coz the express package returns a function
 const app = express();
+
+// by default, response from express is not parsed so use middleware and parse all responses
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/add-product', (req, res)=>{
-    res.send('<h1>This is the add product page route</h1>');
+    res.send('<form action="/product"><input type="submit" value="submit"></form>');
+
+});
+app.use('/product', (req, res)=>{
+    console.log(req.body);
+    res.redirect('/');
 
 });
 app.use('/', (req, res, next)=>{

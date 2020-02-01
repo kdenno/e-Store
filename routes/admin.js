963faf368d4e5d038118473f777b72b39b7c21e3
kdenno <kabuyedennis3@gmail.com>
2020-02-01 16:23:07 +0300

    filtering routes with the same pretext

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res)=>{
-    res.send('<form action="/product" method="POST"><input type="submit" value="submit"></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type="submit" value="submit"></form>');
 
 });
-router.post('/product', (req, res)=>{
+router.post('/add-product', (req, res)=>{
     console.log(req.body);
     res.redirect('/');
 

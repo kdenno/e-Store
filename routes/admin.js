67e09d406af9518ce74ed1ac89190b8688bf1cc7
kdenno <kabuyedennis3@gmail.com>
2020-02-01 16:40:03 +0300

    serving html pages and creating paths

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/add-product', (req, res)=>{
-    res.send('<form action="/admin/add-product" method="POST"><input type="submit" value="submit"></form>');
+const path = require('path');
 
+router.get('/add-product', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 router.post('/add-product', (req, res)=>{
     console.log(req.body);

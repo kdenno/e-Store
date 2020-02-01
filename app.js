bed53347da8fcb772bdf0889240dc77e694a5ba8
kdenno <kabuyedennis3@gmail.com>
2020-02-01 15:12:11 +0300

    creating routes with middleware

--- app.js
+++ app.js
@@ -4,15 +4,12 @@ const fs = require('fs');
 const express = require('express');
 // execute express coz the express package returns a function
 const app = express();
-// create middleware using the .use();
-app.use((req, res, next)=>{
-    console.log('in the middleware');
-    // allow the request to proceed by calling the next(); without it the request would never  jump out of this function
-    next();
+app.use('/add-product', (req, res)=>{
+    res.send('<h1>This is the add product page route</h1>');
 
 });
-app.use((req, res, next)=>{
-    res.send('this part of the file is now reachable');
+app.use('/', (req, res, next)=>{
+    res.send('home page route');
    
 });
 app.listen(8000);

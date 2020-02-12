af39db0e670e6e973dd925fa2d19410b4d1be605
kdenno <kabuyedennis3@gmail.com>
2020-02-12 18:17:04 +0300

    Adding new products to cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,73 +2,104 @@ const Product = require("../models/product");
 const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll().then(products=>{
-    res.render("shop/product-list", {
-      prods: products,
-      pageTitle: "Shop",
-      path: "products"
+  Product.findAll()
+    .then(products => {
+      res.render("shop/product-list", {
+        prods: products,
+        pageTitle: "Shop",
+        path: "products"
+      });
+    })
+    .catch(err => {
+      console.log(err);
     });
-  }).catch(err => {
-    console.log(err);
-  });
- 
 };
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.prodId;
-  Product.findByPk(prodId).then(product => {
-    res.render("shop/product-detail", {
-      product: product,
-      pageTitle: product.title,
-      path: "products"
+  Product.findByPk(prodId)
+    .then(product => {
+      res.render("shop/product-detail", {
+        product: product,
+        pageTitle: product.title,
+        path: "products"
+      });
+    })
+    .catch(err => {
+      console.log("err");
     });
-  }).catch(err => {
-    console.log('err');
-  });
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.findAll().then(products => {
-    res.render("shop/index", { prods: products, pageTitle: "Shop", path: "/" });
-  }).catch(err => {
-    console.log(err);
-  });
+  Product.findAll()
+    .then(products => {
+      res.render("shop/index", {
+        prods: products,
+        pageTitle: "Shop",
+        path: "/"
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 exports.getCart = (req, res, next) => {
   // get all cart products
-      
-      req.user.getCart().then(cart => {
-        return cart.getProducts();// getProducts is a magic method that was created by the relationship
-      
-      })
-      .then(products => {
-        res.render("shop/cart", {
-          path: "cart",
-          products: products,
-          pageTitle: "Cart"
-        });
-        
-      }).catch(err => console.log(err));
 
-  
-  
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart.getProducts(); // getProducts is a magic method that was created by the relationship
+    })
+    .then(products => {
+      res.render("shop/cart", {
+        path: "cart",
+        products: products,
+        pageTitle: "Cart"
+      });
+    })
+    .catch(err => console.log(err));
 };
 exports.postCart = (req, res, next) => {
   const productid = req.body.prodId;
-  // get product price
-  Product.fetchProduct(productid, product => {
-    Cart.addProduct(productid, product.price);
-    res.redirect("/cart");
-  });
+  let fetchedCart;
+  // get user's cart
+  req.user
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      // check if product is available in cart
+      return cart.getProducts({ where: { id: productid } });
+    })
+    .then(product => {
+      let cartproduct;
+      if (product.length > 0) {
+        cartproduct = product[0];
+      }
+      const newquantity = 1;
+      if (cartproduct) {
+        // product exists, update its quantity
+      }
+      // current product is not in the cart, include it and set its quantity
+      return Product.findByPk(productid)
+        .then(product => {
+          fetchedCart.addProduct(product, {
+            through: { quantity: newquantity }
+          }); // add product to cart and update the intermediarry table
+        })
+        .catch(err => console.log(err));
+    })
+    .then(() => {
+      res.redirect("/cart");
+    })
+    .catch(err => console.log(err));
 };
 exports.deleteCartItem = (req, res, next) => {
   const prodId = req.body.productId;
   // get product
   Product.fetchProduct(prodId, product => {
-
     Cart.deleteCartProduct(prodId, product.price);
-    res.redirect('/cart');
-
+    res.redirect("/cart");
   });
 };
 exports.getCheckout = (req, res, next) => {

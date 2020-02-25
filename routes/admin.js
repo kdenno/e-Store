71a728bc7905640e8632276f4d856bab2ffefbdb
kdenno <kabuyedennis3@gmail.com>
2020-02-25 11:24:58 +0300

    route protection with middleware

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const adminController = require('../controllers/admin');
+const adminController = require("../controllers/admin");
+const isAuth = require("../middleware/is-auth");
 
 // /admin/add-product
-router.get('/add-product', adminController.addProduct);
-router.get('/products', adminController.getProducts);
-router.post('/add-product', adminController.createProduct);
-router.get('/edit-product/:productId', adminController.editProduct);
-router.post('/update-product', adminController.updateProduct);
- router.post('/delete-product', adminController.deleteProduct);
+router.get("/add-product", isAuth, adminController.addProduct);
+router.get("/products", adminController.getProducts);
+router.post("/add-product", isAuth, adminController.createProduct);
+router.get("/edit-product/:productId", isAuth, adminController.editProduct);
+router.post("/update-product", isAuth, adminController.updateProduct);
+router.post("/delete-product", isAuth, adminController.deleteProduct);
 
-exports.routes = router; 
+exports.routes = router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,15 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const shopController = require("../controllers/shop");
+const isAuth = require("../middleware/is-auth");
 
- router.get('/', shopController.getIndex);
-router.get('/cart', shopController.getCart);
-router.post('/cart', shopController.postCart);
-router.post('/create-order', shopController.createOrder);
-router.post('/cart-delete-item', shopController.deleteCartItem);
-router.get('/products', shopController.getProducts);
- router.get('/products/:prodId', shopController.getProduct);
-router.get('/orders', shopController.getOrders);
+router.get("/", shopController.getIndex);
+router.get("/cart", isAuth, shopController.getCart);
+router.post("/cart", isAuth, shopController.postCart);
+router.post("/create-order", isAuth, shopController.createOrder);
+router.post("/cart-delete-item", isAuth, shopController.deleteCartItem);
+router.get("/products", shopController.getProducts);
+router.get("/products/:prodId", shopController.getProduct);
+router.get("/orders", isAuth, shopController.getOrders);
 // router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;

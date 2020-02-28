0dda0ae08a65e0a838e896023347c948e41b28be
kdenno <kabuyedennis3@gmail.com>
2020-02-28 15:30:23 +0300

    adding validation with express-validator

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
 const nodemailer = require("nodemailer");
 const sendgridTransport = require("nodemailer-sendgrid-transport");
+const { validationResult } = require("express-validator/check"); // collect all errors that .isEmail collected
 
 const transporter = nodemailer.createTransport({
   auth: {
@@ -82,6 +83,14 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.status(442).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()
+    });
+  }
   User.findOne({ email: email })
     .then(result => {
       if (result) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const { check } = require("express-validator/check");
 const authController = require("../controllers/authController");
 router.get("/login", authController.getLogin);
-router.get('/signup', authController.getSignup);
+router.get("/signup", authController.getSignup);
 router.post("/login", authController.postLogin);
 router.post("/logout", authController.postLogout);
-router.post('/signup', authController.postSignup);
-router.get('/reset', authController.getReset);
-router.post('/reset', authController.postReset);
-router.get('/reset/:token', authController.getNewPassword);
-router.post('/new-password', authController.postNewPassword);
+router.post("/signup", check("email").isEmail(), authController.postSignup);
+router.get("/reset", authController.getReset);
+router.post("/reset", authController.postReset);
+router.get("/reset/:token", authController.getNewPassword);
+router.post("/new-password", authController.postNewPassword);
 
 module.exports = router;
